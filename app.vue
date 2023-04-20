fcd94ddefd13aefd064986c870d4ba43357abe4b
何向阳 <hexiangyang@HIH-L-12035.local>
2023-04-20 20:40:30 +0800

    .

--- app.vue
+++ app.vue
@@ -26,7 +26,8 @@ useHead({
         {
             name: 'viewport',
             content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
-        },
+      
+          },
     ],
 });
 
